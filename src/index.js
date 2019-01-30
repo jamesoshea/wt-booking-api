c663d58d38396957b083d93b88b7e7a763dcb4d1
Ondrej Chmelar <ondrej.chmelar@fragaria.cz>
2019-01-30 12:18:46 +0100

    feat(airlines): improve mailer segment initialization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const { app } = require('./app');
 const adapter = require('./services/adapters/base-adapter');
-const hotelAdapter = require('./services/adapters/hotel-adapter');
-const airlineAdapter = require('./services/adapters/airline-adapter');
 const mailer = require('./services/mailer');
 const { AIRLINE_SEGMENT_ID, HOTEL_SEGMENT_ID } = require('./constants');
 let { config } = require('./config');
 
 if (config.segment === HOTEL_SEGMENT_ID) {
+  const hotelAdapter = require('./services/adapters/hotel-adapter');
   adapter.set(new hotelAdapter.WTHotelAdapter(config.adapterOpts));
 } else if (config.segment === AIRLINE_SEGMENT_ID) {
+  const airlineAdapter = require('./services/adapters/airline-adapter');
   adapter.set(new airlineAdapter.WTAirlineAdapter(config.adapterOpts));
 } else {
   throw new Error(`Unknown segment ${config.segment}`);
