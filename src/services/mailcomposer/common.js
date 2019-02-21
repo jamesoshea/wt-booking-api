5e3cde2422e351aed84eed3abcbeb8b71837ab28
Jirka Chadima <chadima.jiri@gmail.com>
2019-02-21 09:33:00 +0100

    refactor: Uses ISO 3166 instead of countries-list

--- src/services/mailcomposer/common.js
+++ src/services/mailcomposer/common.js
@@ -1,11 +1,11 @@
-const countriesList = require('countries-list');
+const countriesList = require('iso-3166-1-alpha-2');
 
 const normalizeAddress = (address) => {
   if (address.countryCode) {
     // eslint-disable-next-line camelcase
     address.country_code = address.countryCode;
-    if (countriesList.countries[address.countryCode] && !address.country) {
-      address.country = countriesList.countries[address.countryCode].name;
+    if (!address.country && countriesList.getCountry(address.countryCode)) {
+      address.country = countriesList.getCountry(address.countryCode);
     }
     delete address.countryCode;
   }
